93122c74d757de44c311c70c10636b1e20ac0cac
ZZ17390 <kaeru_yuya@hotmail.com>
2018-07-20 16:51:21 +0900

    research pc commit

--- src/export/ErrProcDataCount.java
+++ src/export/ErrProcDataCount.java
@@ -9,6 +9,7 @@ import creator.template.SimpleTemplate;
 import file.CSVFileReadWrite;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,12 +23,12 @@ import param.KomatsuDataParameter;
  */
 public class ErrProcDataCount {
     public static void main(String[] args) {
-        Map<String, List<Integer>> total = new HashMap();
+        Map<String, Integer[]> total = new HashMap();
         int cnt = 0;
         StringBuilder header = new StringBuilder();
         
         for(String errsource : KomatsuDataParameter.ERR_SOURCE){
-            header.append(errsource);
+            header.append(errsource+",");
             Map<String, SimpleTemplate> err = new SyaryoTemplateToJson().reader("error_proc\\"+errsource+"_error.json");
             for(String name : err.keySet()){
                 System.out.println(name);
@@ -39,24 +40,24 @@ public class ErrProcDataCount {
                 }
                     
                 if(total.get(kisy) == null){
-                    total.put(kisy, new ArrayList<>());
+                    total.put(kisy, new Integer[4]);
                     for(int i=0; i < KomatsuDataParameter.ERR_SOURCE.length; i++)
-                        total.get(kisy).add(0);
+                        total.get(kisy)[i] = 0;
                 }
                 
                 int errcnt = Integer.valueOf(err.get(name).name.get(0));
-                total.get(kisy).set(cnt, total.get(kisy).get(0)+errcnt);
+                total.get(kisy)[cnt] = total.get(kisy)[cnt]+errcnt;
             }
             
             cnt++;
         }
         
         try(PrintWriter csv = CSVFileReadWrite.writer("err_procSoruceCount.csv")){
-            csv.println("KISY,"+String.join(",", header)+",Total");
+            csv.println("KISY,"+header.toString()+"Total");
             
             for(String kisy : total.keySet()){
-                String row = total.get(kisy).stream().map(s -> s.toString()).collect(Collectors.joining(","));
-                Integer t = total.get(kisy).stream().mapToInt(s -> s).sum();
+                String row = Arrays.asList(total.get(kisy)).stream().map(s -> s.toString()).collect(Collectors.joining(","));
+                Integer t = Arrays.asList(total.get(kisy)).stream().mapToInt(s -> s).sum();
                 csv.println(kisy+","+row+","+t);
             }
         }
