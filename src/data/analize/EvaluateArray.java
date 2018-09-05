fec9b71880b442f91f487fc96d9b061ea35a95da
ZZ17390 <kaeru_yuya@hotmail.com>
2018-09-05 18:16:44 +0900

    research pc commit

--- src/data/analize/EvaluateArray.java
+++ src/data/analize/EvaluateArray.java
@@ -6,7 +6,8 @@
 package data.analize;
 
 import file.CSVFileReadWrite;
-import file.UserDefinedFile;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,30 +15,35 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import json.MapIndexToJSON;
 import json.SyaryoToZip3;
 import obj.SyaryoObject4;
 import param.KomatsuDataParameter;
-import param.KomatsuUserParameter;
 
 /**
- *
+ * 評価行列の作成
+ *  /  |code.A  code.B|
+ *sid.A|   0      1   |
+ *sid.B|   1      0   |
+ * 現状 KOMTRAX_ERROR 作業のみから作成
  * @author ZZ17390
  */
-public class ErrorServiceCorrelation {
-
-    private static final String exportFile = "ExportData_PC200_ALL.json";
+public class EvaluateArray {
+    private static final String KISY = "PC200";
+    private static final String exportFile = "ExportData_"+KISY+"_ALL.json";
 
     public static void main(String[] args) {
         Map<String, SyaryoObject4> syaryoMap = new SyaryoToZip3().readJSON(exportFile);
 
         SyaryoObject4 dataHeader = syaryoMap.get("_headers");
         syaryoMap.remove("_headers");
-        List usererrors = UserDefinedFile.filter(KomatsuUserParameter.PC200_ERRFILTER_FILE);
-        List kmerrors = new ArrayList(KomatsuDataParameter.PC_ERROR.keySet());
+        //List usererrors = UserDefinedFile.filter(KomatsuUserParameter.PC200_ERRFILTER_FILE);
+        //List kmerrors = new ArrayList(KomatsuDataParameter.PC_ERROR.keySet());
         
         datafilter(syaryoMap, dataHeader);
-        evalArray(syaryoMap, dataHeader, kmerrors, kmerrors);
+        
+        //evalArray(syaryoMap, dataHeader, kmerrors, kmerrors);
+        evalArray(syaryoMap, dataHeader, null, null);
+        //testEvalArray();
     }
 
     private static void datafilter(Map<String, SyaryoObject4> syaryoMap, SyaryoObject4 dataHeader) {
@@ -73,8 +79,8 @@ public class ErrorServiceCorrelation {
     }
 
     private static void evalArray(Map<String, SyaryoObject4> syaryoMap, SyaryoObject4 dataHeader, List<String> error, List<String> service) {
-        try (PrintWriter pw = CSVFileReadWrite.writer(exportFile + "_kmerrcd_evalarray.csv");
-            PrintWriter pw2 = CSVFileReadWrite.writer(exportFile + "_sgcd_evalarray.csv")) {
+        try (PrintWriter pw = CSVFileReadWrite.writer(KISY + "_kmerrcd_evalarray.csv");
+            PrintWriter pw2 = CSVFileReadWrite.writer(KISY + "_sgcd_evalarray.csv")) {
             int err_idx = dataHeader.get("KOMTRAX_ERROR").get("KOMTRAX_ERROR").indexOf("ERROR_CODE");
             int sg_idx = dataHeader.get("作業").get("作業").indexOf("SGYOCD");
 
@@ -98,7 +104,7 @@ public class ErrorServiceCorrelation {
 
                 //作業
                 s.get("作業").values().stream().map(sg -> sg.get(sg_idx).toString()).forEach(cd -> {
-                    String dev = cd.substring(0, 4);
+                    String dev = cd;//.substring(0, 4);
                     
                     if (devcd.get(dev) == null) {
                         devcd.put(dev, new Integer[syaryoMap.size()]);
@@ -119,7 +125,7 @@ public class ErrorServiceCorrelation {
             }
             
             error = error.stream().sorted().collect(Collectors.toList());
-            service = service.stream().map(sv -> sv.substring(0, 4)).sorted().collect(Collectors.toList());
+            service = service.stream().map(sv -> sv/*.substring(0, 4)*/).sorted().collect(Collectors.toList());
             pw.println("コード,"+String.join(",", error));
             pw2.println("コード,"+String.join(",", service));
             for (String name : syaryoMap.keySet()) {
@@ -129,6 +135,66 @@ public class ErrorServiceCorrelation {
                 str = service.stream().map(dev -> devcd.get(dev)==null?"N/A":devcd.get(dev)[names.indexOf(name)].toString()).collect(Collectors.toList());
                 pw2.println(name+","+String.join(",", str));
             }
+        }
+    }
+    
+    private static void testEvalArray(){
+        try(BufferedReader csv1 = CSVFileReadWrite.reader(KISY + "_kmerrcd_evalarray.csv");
+            BufferedReader csv2 = CSVFileReadWrite.reader(KISY + "_sgcd_evalarray.csv")){
+            
+            String[] header1 = csv1.readLine().split(",");
+            String[] header2 = csv2.readLine().split(",");
+            
+            //Header
+            System.out.println("kmerr:"+(header1.length-1));
+            System.out.println("sgdev:"+(header2.length-1));
+            System.out.println("--------");
+            
+            String line;
+            Map<String, List<Integer>> map1 = new HashMap();
+            while((line = csv1.readLine()) != null){
+                String[] data = line.split(",");
+                for(int i=1; i < data.length ; i++){
+                    if(map1.get(header1[i]) == null)
+                        map1.put(header1[i], new ArrayList());
+                    map1.get(header1[i]).add(Integer.valueOf(data[i]));
+                }
+            }
+            //Map1 Sum
+            map1.entrySet().stream()
+                            .map(d -> d.getKey()+":"+d.getValue().stream().mapToInt(l -> l).sum())
+                            .forEach(System.out::println);
+            System.out.println("Total1 = "+map1.values().stream().mapToInt(l -> l.stream().mapToInt(d -> d).sum()).sum());
+            System.out.println("--------");
+            
+            Map<String, List<Integer>> map2 = new HashMap();
+            while((line = csv2.readLine()) != null){
+                String[] data = line.split(",");
+                for(int i=1; i < data.length ; i++){
+                    if(map2.get(header2[i]) == null)
+                        map2.put(header2[i], new ArrayList());
+                    map2.get(header2[i]).add(Integer.valueOf(data[i]));
+                }
+            }
+            //Map2 Sum
+            map2.entrySet().stream()
+                            .map(d -> d.getKey()+":"+d.getValue().stream().mapToInt(l -> l).sum())
+                            .forEach(System.out::println);
+            System.out.println("Total2 = "+map2.values().stream().mapToInt(l -> l.stream().mapToInt(d -> d).sum()).sum());
+            System.out.println("--------");
+            
+            //map1 ∧ map2
+            List<String> m1andm2 = map1.keySet().stream().filter(d -> map2.get(d.substring(0,4))!=null).collect(Collectors.toList());
+            System.out.println("kmerr:"+m1andm2.size());
+            System.out.println("sgdev:"+m1andm2.stream().map(s -> s.substring(0,4)).distinct().count());
+            m1andm2.stream().map(k -> k+":"+map1.get(k).stream().mapToInt(d->d).sum()+","+map2.get(k.substring(0, 4)).stream().mapToInt(d->d).sum()).forEach(System.out::println);
+            System.out.println("1&2Total = "
+                                    + m1andm2.stream().mapToInt(k -> map1.get(k).stream().mapToInt(d->d).sum()).sum()+","
+                                    + m1andm2.stream().map(k ->k.substring(0,4)).distinct().mapToInt(dev -> map2.get(dev).stream().mapToInt(d->d).sum()).sum());
+            System.out.println("--------");
+            
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 }
