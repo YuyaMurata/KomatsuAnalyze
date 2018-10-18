bb5e2dc977ed0cceb73dbe4430776b9525f3386c
ZZ17390 <kaeru_yuya@hotmail.com>
2018-10-18 17:47:43 +0900

    research pc commit

--- src/data/ga/GeneticAlgorithm.java
+++ src/data/ga/GeneticAlgorithm.java
@@ -8,11 +8,13 @@ package data.ga;
 import data.analize.EvaluateCorrelation;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Random;
 import java.util.TreeMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import org.apache.hive.druid.org.jboss.netty.util.internal.ConcurrentHashMap;
@@ -22,52 +24,73 @@ import org.apache.hive.druid.org.jboss.netty.util.internal.ConcurrentHashMap;
  * @author kaeru_yuya
  */
 public class GeneticAlgorithm {
-    private Random RAND = new Random();
-    private Integer LOOP_N = 100;
-    private Integer N = 100;
+
+    private static Random RAND = new Random();
+    private static Integer LOOP_N = 1000;
+    private Integer GEN_N = 20;
     private Double MUPB = 0.1;
-    private Double CXPB = 0.8;
-    private Double UNICX = 0.5;
-    private Integer ELITE_G = 4;
-    private static Integer len = 40;
+    private Double CXPB = 1.0;
+    private Double UNICX = 0.3;
+    private int ELITE_G = (int) (0.1 * GEN_N);
+    private static Integer GEN_LEN = 10;
     private double[][] fit;
     private double[] tg;
-    private Map<List<Integer>, Double> POOL =new ConcurrentHashMap();
-    
+    private List<Population> POOL = new CopyOnWriteArrayList<>();
+    public static Double best;
+
     public static void main(String[] args) {
-        GeneticAlgorithm ga = new GeneticAlgorithm();
-        ga.settings(new double[len][10], new double[10]);
-        ga.initialize(ga.N);
-    }
-    
-    public void settings(double[][] matrix, double[] target){
-        len = matrix.length;
-        fit = matrix;
-        tg = target;
-        
-        //test
-        for(int i=0; i < len; i++)
-            for(int j=0; j < 10; j++)
-                fit[i][j] = i+j;
-    }
-    
-    //初期個体の生成
-    public void initialize(int n){
-        while(POOL.size() < n){
-            List<Integer> gene = new ArrayList();
-            IntStream.range(0, len).map(i -> RAND.nextInt(2)).forEach(gene::add);
-            POOL.put(gene, 0d);
+        double totalavg = 0;
+        for (int n = 0; n < 1000; n++) {
+            GeneticAlgorithm ga = new GeneticAlgorithm();
+            ga.settings(new double[GEN_LEN][10], new double[10]);
+            ga.initialize(ga.GEN_N);
+            best = 0d;
+
+            for (int i = 0; i < LOOP_N; i++) {
+                ga.next();
+                if (best == 10) {
+                    break;
+                }
+            }
+            
+            totalavg += ga.cnt;
         }
         
-        fitness();
-        
-        //Test
-        POOL.entrySet().stream().map(e -> e.getKey()+":"+e.getValue()).forEach(System.out::println);
+        System.out.println(totalavg / 1000);
     }
-    
+
+    public void settings(double[][] matrix, double[] target) {
+        GEN_LEN = matrix.length;
+        fit = matrix;
+        tg = target;
+
+        //test
+        for (int i = 0; i < GEN_LEN; i++) {
+            for (int j = 0; j < 10; j++) {
+                fit[i][j] = i + j;
+            }
+        }
+    }
+
+    //初期個体の生成
+    public void initialize(int n) {
+        Map<List<Integer>, Double> initPOOL = new ConcurrentHashMap();
+
+        while (initPOOL.size() < n) {
+            List<Integer> gene = new ArrayList();
+            IntStream.range(0, GEN_LEN).map(i -> RAND.nextInt(2)).forEach(gene::add);
+            initPOOL.put(gene, -1d);
+        }
+
+        //Test
+        initPOOL.entrySet().stream()
+            .map(g -> new Population(g.getKey(), g.getValue())).forEach(POOL::add);
+    }
+
     //適応値の計算
-    public void fitness(){
-        POOL.entrySet().parallelStream().forEach(g ->{
+    public void fitness() {
+        POOL.parallelStream().forEach(p -> {
+            /*
             double[] farray = new double[fit[0].length];
             Arrays.fill(farray, 0d);
             
@@ -80,60 +103,104 @@ public class GeneticAlgorithm {
             
             //相関 正値と負値をとるため
             double f = Math.abs(EvaluateCorrelation.correlation(tg, farray));
-            
-            g.setValue(f);
+             */
+            //test
+
+            p.f = IntStream.range(0, GEN_LEN).filter(i -> p.g.get(i) == 1).mapToDouble(i -> 1d).sum();
         });
     }
-    
-    public List selection(){
+
+    public List<Population> selection() {
         //エリート選択
-        List<List<Integer>> select = POOL.entrySet().stream()
-                                            .sorted(Entry.comparingByValue())
-                                            .limit(ELITE_G)
-                                            .map(e -> e.getKey())
-                                            .collect(Collectors.toList());
+        List<Population> select = POOL.stream()
+            .sorted(Comparator.comparing(Population::getFit).reversed())
+            .limit(ELITE_G)
+            .collect(Collectors.toList());
+        POOL.removeAll(select);
         
+        if(!select.isEmpty())
+            best = select.get(0).f;
+        else
+            best = POOL.stream().mapToDouble(p -> p.f).max().getAsDouble();
+
         //ルーレット選択 (POOL size - Elite size)
-        TreeMap<Double, List<Integer>> roullet = new TreeMap();
+        TreeMap<Double, Population> roullet = new TreeMap();
         Double acm = 0d;
-        double sum = POOL.entrySet().parallelStream().filter(g -> !select.contains(g.getKey()))
-                                                .mapToDouble(g -> g.getValue()).sum();
-        for(List<Integer> key : POOL.keySet().stream().filter(g -> !select.contains(g)).collect(Collectors.toList())){
-            roullet.put(acm, key);
-            acm += POOL.get(key) / sum;
+        double sum = POOL.parallelStream().mapToDouble(p -> p.f).sum();
+        for (Population p : POOL) {
+            roullet.put(acm, p);
+            acm += p.f / sum;
         }
-        while(select.size() < N)
+
+        while (select.size() < GEN_N) {
             select.add(roullet.floorEntry(RAND.nextDouble()).getValue());
-        
+        }
+
         return select;
     }
-    
+
     //交叉 (一様交叉)
-    public void crossover(List<Integer> p1, List<Integer> p2){
-        if(RAND.nextDouble() >= CXPB)
-            return;
-        
-        List<Integer> child = new ArrayList<>();
-        IntStream.range(0, p1.size()).parallel()
-                .filter(i -> RAND.nextDouble() >= UNICX)
-                .forEach(i -> {
-                    Integer sw = p1.get(i);
-                    p1.set(i, p2.get(i));
-                    p2.set(i, sw);
-                });
+    public Population crossover(Population p1, Population p2) {
+        Population child = new Population(p1.g, p1.f);
+
+        if (RAND.nextDouble() >= CXPB) {
+            return child;
+        }
+
+        IntStream.range(0, p1.g.size()).parallel()
+            .filter(i -> RAND.nextDouble() >= UNICX)
+            .forEach(i -> {
+                child.g.set(i, p2.g.get(i));
+            });
+
+        return child;
     }
-    
+
     //突然変異
-    public void mutable(List<Integer> p){
-        if(RAND.nextDouble() >= MUPB)
-            return ;
-            
-        IntStream.range(0, p.size()).parallel()
-                            .filter(i -> RAND.nextDouble() < MUPB)
-                            .forEach(i -> p.set(i, (p.get(i)+1)%2));
-    }
-    
-    public void next(){
+    public Population mutable(Population p) {
+        Population child = new Population(p.g, p.f);
+
+        if (RAND.nextDouble() >= MUPB) {
+            return child;
+        }
         
+        int r = RAND.nextInt(p.g.size());
+        /*IntStream.range(0, p.g.size()).parallel()
+            .filter(i -> RAND.nextDouble() < MUPB)
+            .forEach(i -> child.g.set(i, (child.g.get(i) + 1) % 2));*/
+        child.g.set(r, (p.g.get(r)+1)%2);
+
+        return child;
+    }
+
+    public int cnt = 0;
+
+    public void next() {
+        //ループ回数
+        cnt++;
+
+        //評価
+        fitness();
+
+        //選択
+        List<Population> nextgene = selection();
+
+        for (int i = ELITE_G; i < nextgene.size(); i++) {
+            //交叉
+            nextgene.set(i, crossover(nextgene.get(i), nextgene.get((i+1) % GEN_N)));
+
+            //突然変異
+            nextgene.set(i, mutable(nextgene.get(i)));     
+        }
+        //次世代の登録
+        POOL = new CopyOnWriteArrayList<>();
+        nextgene.stream().forEach(POOL::add);
+
+        //testPrint();
+    }
+
+    private void testPrint() {
+        System.out.println("------  N="+cnt+"  ------");
+        System.out.println(cnt + "," + POOL.stream().map(p -> p.f.toString()).collect(Collectors.joining(",")));
     }
 }
---
+++ src/data/ga/Population.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data.ga;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author ZZ17390
+ */
+public class Population {
+    public List<Integer> g = new ArrayList();
+    public Double f = 0d;
+
+    public Population(List g, Double f) {
+        this.g = new ArrayList(g);
+        this.f = f;
+    }
+    
+    public Double getFit(){
+        return f;
+    }
+    
+    public String toString() {
+        return g+":"+f;
+    }
+}
