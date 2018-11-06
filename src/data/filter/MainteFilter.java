fafe66e03d5ea71f8369a45f562c59ffbbe1e8ff
ZZ17390 <kaeru_yuya@hotmail.com>
2018-11-06 16:07:59 +0900

    research pc commit

--- src/data/filter/MainteFilter.java
+++ src/data/filter/MainteFilter.java
@@ -17,24 +17,50 @@ public class MainteFilter {
     private static Map<String, List> mainte = KomatsuDataParameter.PERIOD_MAINTE;
     private static Integer priceTh = 5000;
     
-    public static Boolean detect(String sgkt, String hnbn, Integer price){
-        //作業形態に基づくフィルタ
-        if(mainte.get("受注.SGYO_KTICD").contains(sgkt))
-            return true;
+    public static Boolean allDetect(String sgkt, String hnbn, Integer price){
+        Boolean f = 
+            skDetect(sgkt, hnbn, price) || 
+            partsDetect(sgkt, hnbn, price) ||
+            kesDetect(sgkt, hnbn, price) ||
+            priceDetect(sgkt, hnbn, price);
         
-        //品番に基づくフィルタ
-        if(mainte.get("部品.HNBN").contains(hnbn))
+        return f;
+    }
+    
+    public static Boolean skDetect(String sgkt, String hnbn, Integer price){
+        //作業形態に基づくフィルタ
+        if(mainte.get("受注.SGYO_KTICD").contains(sgkt)){
             return true;
-            
+        }
+
+        return false;
+    }
+    
+    public static Boolean partsDetect(String sgkt, String hnbn, Integer price){    
+        //品番に基づくフィルタ
+        if(mainte.get("部品.HNBN").contains(hnbn)){
+            return true;
+        }
+        
+        return false;
+    }
+    
+    public static Boolean kesDetect(String sgkt, String hnbn, Integer price){   
         //KES部品のフィルタ
         String[] kes = hnbn.split("-");
         if(kes.length == 2)
-            if(kes[0].length() == 5 && kes[1].length() == 5)
+            if(kes[0].length() == 5 && kes[1].length() == 5){
                 return true;
+            }
         
+        return false;
+    }
+    
+    public static Boolean priceDetect(String sgkt, String hnbn, Integer price){
         //金額フィルタ
-        if(price < priceTh)
+        if(price < priceTh){
             return true;
+        }
         
         return false;
     }
