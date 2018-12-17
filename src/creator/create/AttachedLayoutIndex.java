0d67802d89c9cfef66aed911f76ccc7ec1d64ba7
kaeru_yuya <kaeru_yuya@192.168.1.11>
2018-12-17 12:18:16 +0900

    PC Commit

---
+++ src/creator/create/AttachedLayoutIndex.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package creator.create;
+
+import java.util.List;
+import java.util.Map;
+import param.KomatsuDataParameter;
+
+/**
+ *
+ * @author kaeru_yuya
+ */
+public class AttachedLayoutIndex {
+    public static void main(String[] args) {
+        Map<String, List> layout = KomatsuDataParameter.DATALAYOUT_INDEX;
+    }
+}
