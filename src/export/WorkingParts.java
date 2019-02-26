f9d4f962b0e62f2e34a897ace0813fa9bfc8bf8b
ZZ17390 <kaeru_yuya@hotmail.com>
2019-02-26 18:19:01 +0900

    research pc commit

--- src/export/WorkingParts.java
+++ src/export/WorkingParts.java
@@ -7,18 +7,17 @@ package export;
 
 import analizer.SyaryoAnalizer;
 import file.CSVFileReadWrite;
-import index.SyaryoObjectElementsIndex;
+import file.MapToJSON;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Optional;
 import java.util.stream.Collectors;
-import file.SyaryoToCompress;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import obj.SyaryoLoader;
 import obj.SyaryoObject;
-import param.KomatsuDataParameter;
 
 /**
  *
@@ -27,75 +26,98 @@ import param.KomatsuDataParameter;
 public class WorkingParts {
 
     //レイアウト
-    private static Map<String, List> dataIndex = SyaryoObjectElementsIndex.getInstance().getIndex();
-    private static String PATH = KomatsuDataParameter.SYARYOOBJECT_FDPATH;
     private static String KISY = "PC200";
-    private static Map<String, SyaryoObject> map;
+    private static SyaryoLoader LOADER = SyaryoLoader.getInstance();
+    private static Map<String, String> survey = new MapToJSON().toMap("user\\PC200調査品番.json");
 
     public static void main(String[] args) {
         //車両の読み込み
-        map = new SyaryoToCompress().read(PATH + "syaryo_obj_" + KISY + "_sv_form.bz2");
-        int sgcdIdx = dataIndex.get("作業").indexOf("SGYOCD");
-        int sgmainIdx = dataIndex.get("作業").indexOf("0");
-        int ksycdIdx = dataIndex.get("部品").indexOf("KSYCD");
-        int pmakerIdx = dataIndex.get("部品").indexOf("None"); //BHN_MAKR_KBN
-        int hnbnIdx = dataIndex.get("部品").indexOf("HNBN");
-        int bhnnmIdx = dataIndex.get("部品").indexOf("BHN_NM");
-        int bhnquantIdx = dataIndex.get("部品").indexOf("JISI_SU");
-        int bhnpriceIdx = dataIndex.get("部品").indexOf("SKKG");
+        LOADER.setFile(KISY + "_form");
 
-        try (PrintWriter pw = CSVFileReadWrite.writer(KISY + "_workingparts.csv")) {
-            //Header
-            pw.println("SID,会社コード,作番(作業が存在するもの),部品メーカ,品番,品名");
+        partsToWorking(LOADER.getSyaryoMap());
+    }
 
-            map.values().stream()
-                .filter(s -> s.get("作業") != null)
-                .filter(s -> s.get("部品") != null)
-                .forEach(s -> {
-                    SyaryoAnalizer analize = new SyaryoAnalizer(s);
-                    List<String> worksbn = analize.getValue("作業", new Integer[]{0}).keySet().stream()
-                        .map(w -> w.split("#")[0])
-                        .distinct()
-                        .collect(Collectors.toList());
+    private static void partsToWorking(Map<String, SyaryoObject> map) {
+        int hnbnIdx = LOADER.index("部品", "HNBN");
+        int bhnnmIdx = LOADER.index("部品", "BHN_NM");
+        int ksycdIdx = LOADER.index("部品", "会社CD");
+        int sgcdIdx = LOADER.index("作業", "SGYOCD");
+        int odrIdx = LOADER.index("受注", "ODR_KBN");
 
-                    worksbn.stream().forEach(ws -> {
-                        Optional<String> sgcheck = analize.getSBNWork(ws).values().stream()
-                            .filter(w -> w.get(sgmainIdx).equals("1"))
-                            .map(w -> w.get(sgcdIdx))
-                            .findFirst();
-                        
-                        if(!sgcheck.isPresent()){
-                            System.out.println(ws+":代表作業なし");
-                            return;
+        Map<String, Integer> sgcd = new HashMap<>();
+        Map<String, List<String>> data = new LinkedHashMap<>();
+        map.values().stream().filter(syaryo -> syaryo.get("受注") != null).forEach(syaryo -> {
+            try (SyaryoAnalizer s = new SyaryoAnalizer(syaryo, false)) {
+                
+                List<String> sbns = s.get().get("受注").entrySet().stream()
+                                    .filter(odr -> odr.getValue().get(odrIdx).equals("2")) //単体販売を除く
+                                    .map(odr -> odr.getKey())
+                                    .collect(Collectors.toList());
+                //System.out.println(s.get().getName()+" - "+sbns.size());
+
+                if (s.get().get("部品") != null) {
+                    sbns.stream().filter(sbn -> s.getSBNParts(sbn) != null).forEach(sbn -> {
+                        List<String> hnbn = s.getSBNParts(sbn).values().stream()
+                            .map(p -> s.get().name + "," + p.get(ksycdIdx) + "," + sbn + "," + p.get(hnbnIdx) + "," + p.get(bhnnmIdx))
+                            .distinct()
+                            .filter(h -> survey.keySet().stream().filter(suv -> h.contains(suv)).findFirst().isPresent())
+                            .map(h -> h + "," + survey.entrySet().stream().filter(suv -> h.contains(suv.getKey())).findFirst().get().getValue())
+                            .collect(Collectors.toList());
+
+                        if (!hnbn.isEmpty()) {
+                            List<String> w;
+                            if (s.getSBNWork(sbn) != null) {
+                                w = s.getSBNWork(sbn).values().stream()
+                                    .map(sg -> sg.get(sgcdIdx))
+                                    .distinct()
+                                    .collect(Collectors.toList());
+                            } else {
+                                w = new ArrayList<>();
+                            }
+                            
+                            //作業コードのまとめ
+                            w.stream().forEach(sg -> {
+                                if (sgcd.get(sg) == null) {
+                                    sgcd.put(sg, 0);
+                                }
+                                sgcd.put(sg, sgcd.get(sg) + 1);
+                            });
+
+                            //データの登録
+                            hnbn.stream().forEach(h -> data.put(h, w));
                         }
-                        
-                        String sgcd = sgcheck.get();
-                        if(analize.getSBNParts(ws) == null){
-                            System.out.println(ws+":部品なし");
-                            return;
-                        }
-                        
-                        Map<String, Integer> sortMap = analize.getSBNParts(ws).entrySet().stream()
-                                                .filter(e -> e.getValue().get(pmakerIdx).equals("10"))
-                                                .collect(Collectors.toMap(e -> e.getKey(), e -> Integer.valueOf(e.getValue().get(bhnpriceIdx)) / Integer.valueOf(e.getValue().get(bhnquantIdx))));
-                        
-                        if(sortMap.isEmpty()){
-                            System.out.println(sgcd+":コマツ部品なし");
-                            return;
-                        }
-                        
-                        String maxSbn = Collections.max(sortMap.entrySet(), Map.Entry.comparingByValue()).getKey();
-                        List<String> parts = analize.get().get("部品").get(maxSbn);
-                        
-                        //金額の高い部品
-                        pw.println(s.name+","+ws+","+sgcd+","+String.join(",", parts));
-                        
-                        //全部品
-                        /*sortMap.keySet().stream().map(pa -> analize.get().get("部品").get(pa)).forEach(pa ->{
-                            pw.println(s.name+","+ws+","+sgcd+","+String.join(",", pa));
-                        });*/
+
                     });
-                });
+                }
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
+
+        sgcd.entrySet().stream().map(sg -> sg.getKey() + "," + sg.getValue()).forEach(System.out::println);
+        System.out.println("total," + data.size());
+        
+        List<String> sgh = new ArrayList<>();
+        //累積比率による出力制限
+        int sum = sgcd.values().stream().mapToInt(sg -> sg).sum();
+        int acm = 0;
+        for(String sg : sgcd.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).map(sg -> sg.getKey()).collect(Collectors.toList())){
+            sgh.add(sg);
+            acm += sgcd.get(sg);
+            if(((double)acm / (double)sum) > 0.9d)
+                break;
+        }
+        
+        try (PrintWriter pw = CSVFileReadWrite.writerSJIS(KISY + "_workingparts.csv")) {
+            //Header
+            String header = "SID,会社コード,作番,品番,品名,調査,";
+            pw.println(header + String.join(",", sgh));
+
+            //出力
+            data.entrySet().stream()
+                .map(d -> d.getKey() + "," + sgh.stream().map(sg -> d.getValue().contains(sg) ? "1" : "0").collect(Collectors.joining(",")))
+                .forEach(pw::println);
         }
     }
 }
