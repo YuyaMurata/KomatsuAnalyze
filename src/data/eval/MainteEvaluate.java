6b059a84833f0b853370ad7ba85c7be7c78e2306
ZZ17390 <kaeru_yuya@hotmail.com>
2019-04-19 18:07:48 +0900

    research pc commit

--- src/data/eval/MainteEvaluate.java
+++ src/data/eval/MainteEvaluate.java
@@ -12,8 +12,6 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import obj.SyaryoLoader;
 import param.KomatsuUserParameter;
