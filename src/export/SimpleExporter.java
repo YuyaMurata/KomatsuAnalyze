2ccee3e95681737b2cd9f7db31cc8ff9130d5e43
ZZ17390 <kaeru_yuya@hotmail.com>
2019-05-16 17:53:10 +0900

    research pc commit

--- src/export/SimpleExporter.java
+++ src/export/SimpleExporter.java
@@ -32,12 +32,12 @@ public class SimpleExporter {
         
         //ヘッダー設定
         Map<String, Integer> headers = new LinkedHashMap();
-        headers.put("受注.会社", LOADER.index("受注", "会社CD"));
-        headers.put("受注.作番", LOADER.index("受注", "KEY"));
-        headers.put("受注.日付", LOADER.index("受注", "ODDAY"));
-        headers.put("受注.作業形態", LOADER.index("受注", "SGYO_KTICD"));
-        headers.put("受注.作業形態名", LOADER.index("受注", "SGKT_NM"));
-        headers.put("受注.概要", LOADER.index("受注", "GAIYO_1"));
+        //headers.put("受注.会社", LOADER.index("受注", "会社CD"));
+        //headers.put("受注.作番", LOADER.index("受注", "KEY"));
+        //headers.put("受注.日付", LOADER.index("受注", "ODDAY"));
+        //headers.put("受注.作業形態", LOADER.index("受注", "SGYO_KTICD"));
+        //headers.put("受注.作業形態名", LOADER.index("受注", "SGKT_NM"));
+        //headers.put("受注.概要", LOADER.index("受注", "GAIYO_1"));
         //headers.put("部品.会社", LOADER.index("部品", "会社CD"));
         //headers.put("部品.作番", LOADER.index("部品", "KEY"));
         //headers.put("部品.品番", LOADER.index("部品", "HNBN"));
@@ -51,9 +51,10 @@ public class SimpleExporter {
         //headers.put("顧客.顧客CD", dataIndex.get("顧客").indexOf("NNSCD"));
         //headers.put("顧客.区分", dataIndex.get("顧客").indexOf("KKYK_KBN"));
         //headers.put("顧客.業種", dataIndex.get("顧客").indexOf("GYSCD"));
+        headers.put("KOMTRAX_ERROR.CODE", LOADER.index("KOMTRAX_ERROR", "ERROR_CODE"));
         
         //フィルタ設定　定期メンテナンス
-        simplefilter = " AT,";
+        simplefilter = "";
         
         System.out.println(headers);
 
@@ -77,7 +78,7 @@ public class SimpleExporter {
                 try (SyaryoAnalizer syaryo = new SyaryoAnalizer(map.get(name), false)) {
                     export(pw, headers, syaryo);
                 } catch (Exception ex) {
-                    ex.printStackTrace();
+                    System.err.println(name+" data is NULL");
                 }
             }
         }
