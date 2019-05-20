e07fa6f7a4aa827038762e87d6b8e4c53e2fbcb0
ZZ17390 <kaeru_yuya@hotmail.com>
2019-05-20 17:22:55 +0900

    research pc commit

---
+++ src/test/TestJSONMAnage.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import java.util.Map;
+import jsonDatabase.JsonDB;
+import jsonDatabase.JsonDataObject;
+
+/**
+ *
+ * @author ZZ17807
+ */
+public class TestJSONMAnage {
+
+    public static void main(String[] args) {        
+        // 接続先のMongoDBの設定
+        String host = "localhost";
+        int port = 27017;
+        JsonDB db = new JsonDB(host, port);
+        db.setCollectionName("komatsuDB");
+        
+        // IDが“aaa-bbb-ccc-ddd”のJsonデータを取得
+        String id = "PC200-10- -450635";
+        JsonDataObject jdo = db.getJsonDataObject(id);
+        
+        System.out.println(jdo.getDataMap());
+        
+        // IDに紐づく車両データを出力
+        Map syaryoData = jdo.getTableData("受注");
+        System.out.println(syaryoData);
+
+    }
+}
