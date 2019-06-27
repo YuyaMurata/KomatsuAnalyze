29678aae803d54bc08cc815d63d0bee76bfb9622
ZZ17390 <kaeru_yuya@hotmail.com>
2019-06-27 18:21:11 +0900

    research pc commit

--- src/export/ClusterLoadMapList.java
+++ src/export/ClusterLoadMapList.java
@@ -8,7 +8,6 @@ package export;
 import file.CSVFileReadWrite;
 import file.ListToCSV;
 import java.io.PrintWriter;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
